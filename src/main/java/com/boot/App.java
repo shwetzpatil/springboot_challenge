ba0ccbfd83521505d3a204791fd670a0295302c2
shweta patil <shwetapatil@shwetas-mbp.home>
2018-11-04 15:03:18 +0000

    initial setup

---
+++ src/main/java/com/boot/App.java
@@ -0,0 +1,13 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main( String[] args)
+    {
+//        System.out.println("Hello world");
+        SpringApplication.run(App.class, args);
+    }
+}
